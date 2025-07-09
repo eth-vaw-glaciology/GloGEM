3be4e75221c62c60a016c6c586874852f03a2733
sdrocer <janosch_beer@gmx.de>
2025-07-09 20:18:15 +0200

    delete idl version of Harry's GloGEMflow there is a separate branch that stores this version

--- flow/scripts/matlab_scripts/variables_renew.m
+++
@@ -1,6 +0,0 @@
-% Renew the variables while the solving of the continuity equation (which
-% is performed in three intermediate steps)
-
-sur(2:xnum)=bed(2:xnum)+th(2:xnum);
-width_surface=width_base(:)+lambda(:).*th(:);
-width_mid=(width_base(:)+width_surface(:))/2;
