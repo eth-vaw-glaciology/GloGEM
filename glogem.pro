eth-vaw-glaciology/GloGEM

commit 44d6b6eb41a8d474fbde7ef692b8565680fcca27

add: implement procedure to prepare output files for mass balance in elevation bins

glogem.pro
@@ -599,54 +599,7 @@ tgs_cum=dblarr(nb)   ; array for storing local air temperatures
 ; prepare output for mass balance in elevation bands
 
 if meltmodel eq '1' then mtt='' else mtt='_m3'
-b='/files'+mtt+'/'+GCM_model[gcms]+'/'+GCM_rcp[rcps]
-if reanalysis_direct eq 'y' then b='/PAST'+mtt
-
-if write_mb_elevationbands eq 'y' then begin
-
-   c=findfile(dirres+dir_region+b+'/mb_elevation')
-   if c[0] eq '' then begin
-      spawn,'mkdir '+dirres+dir_region+b+'/mb_elevation' & spawn,'chmod a+rx '+dirres+dir_region+b+'/mb_elevation'
-   endif
-
-   openw,8,dirres+dir_region+b+'/mb_elevation/belev_'+id[gg[g]]+'.dat'
-   a='' & for i=0,years-1 do a=a+string(i+tran[0],fo='(i4)')+'  '
-   printf,8,'Elev  '+a+a
-   elev_bmb=dblarr(years,nb)+snoval & elev_bwb=elev_bmb 
-
-   ; elevation-specified refreezing files 
-   c=findfile(dirres+dir_region+b+'/refr_elevation')
-   if c[0] eq '' then begin
-      spawn,'mkdir '+dirres+dir_region+b+'/refr_elevation' & spawn,'chmod a+rx '+dirres+dir_region+b+'/refr_elevation'
-   endif
-   openw,40,dirres+dir_region+b+'/refr_elevation/refrelev_'+id[gg[g]]+'.dat'
-   a='' & for i=0,years-1 do a=a+string(i+tran[0],fo='(i4)')+'  '
-   printf,40,'Elev  '+a &  elev_refr=dblarr(years,nb)+snoval 
-
-   if debris_supraglacial eq 'y' then begin
-   ; elevation-specified debris files 
-      c=findfile(dirres+dir_region+b+'/debris_elevation')
-      if c[0] eq '' then begin
-         spawn,'mkdir '+dirres+dir_region+b+'/debris_elevation' & spawn,'chmod a+rx '+dirres+dir_region+b+'/debris_elevation'
-      endif
-      openw,41,dirres+dir_region+b+'/debris_elevation/debthick_'+id[gg[g]]+'.dat'
-      a='' & for i=0,years-1 do a=a+string(i+tran[0],fo='(i4)')+'  '
-      printf,41,'Elev  '+a &  elev_debthick=dblarr(years,nb)+snoval 
-
-      openw,42,dirres+dir_region+b+'/debris_elevation/debfrac_'+id[gg[g]]+'.dat'
-      printf,42,'Elev  '+a &  elev_debfrac=dblarr(years,nb)+snoval 
-
-      openw,43,dirres+dir_region+b+'/debris_elevation/debfactor_'+id[gg[g]]+'.dat'
-      printf,43,'Elev  '+a &  elev_debfactor=dblarr(years,nb)+snoval 
-
-      openw,44,dirres+dir_region+b+'/debris_elevation/pondarea_'+id[gg[g]]+'.dat'
-      printf,44,'Elev  '+a &  elev_pondarea=dblarr(years,nb)+snoval 
-
-      if eval_mbelevsensitivity eq 'y' then begin
-         openw,44,dirres+dir_region+b+'/debris_elevation/mbsensitivity_'+id[gg[g]]+'.dat'
-         printf,44,'Elev  '+a &  elev_mbsens=dblarr(years,nb)+snoval &  elev_mbsensall=dblarr(count_mbelevsens_v0+1,years,nb)+snoval 
-      endif
-   endif
+   @procedures/write/prepare_output_mb_in_bins.pro
 endif
 
 ;PRINT, b
@@ -1411,7 +1364,7 @@ endif
 
 endfor                          ; RCPs
 
-endfor                          ; GCMs
+                                ; GCMs
 
 if plot eq 'y' or areaplot eq 'y' then device,/close_file
 

procedures/write/prepare_output_mb_in_bins.pro
@@ -1,0 +1,57 @@
+; *************************************************************
+; *  Prepare output files for mass balance in elevation bins  *
+; *************************************************************
+; This procedure prepares the output files for mass balance in elevation bins. 
+; It creates the necessary directories and files, and writes the headers for the output data. 
+; The actual data will be written in a later step.
+
+compile_opt idl2
+
+b='/files'+mtt+'/'+GCM_model[gcms]+'/'+GCM_rcp[rcps]
+if reanalysis_direct eq 'y' then b='/PAST'+mtt
+
+if write_mb_elevationbands eq 'y' then begin
+
+   c=findfile(dirres+dir_region+b+'/mb_elevation')
+   if c[0] eq '' then begin
+      spawn,'mkdir '+dirres+dir_region+b+'/mb_elevation' & spawn,'chmod a+rx '+dirres+dir_region+b+'/mb_elevation'
+   endif
+
+   openw,8,dirres+dir_region+b+'/mb_elevation/belev_'+id[gg[g]]+'.dat'
+   a='' & for i=0,years-1 do a=a+string(i+tran[0],fo='(i4)')+'  '
+   printf,8,'Elev  '+a+a
+   elev_bmb=dblarr(years,nb)+snoval & elev_bwb=elev_bmb 
+
+   ; elevation-specified refreezing files 
+   c=findfile(dirres+dir_region+b+'/refr_elevation')
+   if c[0] eq '' then begin
+      spawn,'mkdir '+dirres+dir_region+b+'/refr_elevation' & spawn,'chmod a+rx '+dirres+dir_region+b+'/refr_elevation'
+   endif
+   openw,40,dirres+dir_region+b+'/refr_elevation/refrelev_'+id[gg[g]]+'.dat'
+   a='' & for i=0,years-1 do a=a+string(i+tran[0],fo='(i4)')+'  '
+   printf,40,'Elev  '+a &  elev_refr=dblarr(years,nb)+snoval 
+
+   if debris_supraglacial eq 'y' then begin
+   ; elevation-specified debris files 
+      c=findfile(dirres+dir_region+b+'/debris_elevation')
+      if c[0] eq '' then begin
+         spawn,'mkdir '+dirres+dir_region+b+'/debris_elevation' & spawn,'chmod a+rx '+dirres+dir_region+b+'/debris_elevation'
+      endif
+      openw,41,dirres+dir_region+b+'/debris_elevation/debthick_'+id[gg[g]]+'.dat'
+      a='' & for i=0,years-1 do a=a+string(i+tran[0],fo='(i4)')+'  '
+      printf,41,'Elev  '+a &  elev_debthick=dblarr(years,nb)+snoval 
+
+      openw,42,dirres+dir_region+b+'/debris_elevation/debfrac_'+id[gg[g]]+'.dat'
+      printf,42,'Elev  '+a &  elev_debfrac=dblarr(years,nb)+snoval 
+
+      openw,43,dirres+dir_region+b+'/debris_elevation/debfactor_'+id[gg[g]]+'.dat'
+      printf,43,'Elev  '+a &  elev_debfactor=dblarr(years,nb)+snoval 
+
+      openw,44,dirres+dir_region+b+'/debris_elevation/pondarea_'+id[gg[g]]+'.dat'
+      printf,44,'Elev  '+a &  elev_pondarea=dblarr(years,nb)+snoval 
+
+      if eval_mbelevsensitivity eq 'y' then begin
+         openw,44,dirres+dir_region+b+'/debris_elevation/mbsensitivity_'+id[gg[g]]+'.dat'
+         printf,44,'Elev  '+a &  elev_mbsens=dblarr(years,nb)+snoval &  elev_mbsensall=dblarr(count_mbelevsens_v0+1,years,nb)+snoval 
+      endif
+   endif

commit fbeda442c9be2ba09ca051649da06f107d11d9f7

fix: restore accidentally deleted endfor for GCMs loop
Commit 44d6b6e inadvertently removed the endfor closing the GCMs
for-loop. This restores it permanently.

Co-Authored-By: Claude Sonnet 4.6 <noreply@anthropic.com>

glogem.pro
@@ -482,28 +482,7 @@ endif
 
 ; ---------------------
 ; attribute specific parameter values
-
-if read_parameters eq 'y' and cal1 eq 0 then begin
-
-   a=min(abs(double(id[gg[g]])-cali_id),ind)
-
- case meltmodel of
-      '1': Begin
-         DDFice=cali_ddfice[ind] & DDFsnow=cali_ddfsnow[ind] & C_prec=cali_cprec[ind]
-         t_offset=cali_toff[ind]
-       end
-      '3': begin
-         C0=cali_c0[ind] & C1=cali_c1[ind] & alb_ice=cali_a_ice[ind] & alb_snow=cali_a_snow[ind]
-         C_prec=cali_cprec[ind] & t_offset=cali_toff[ind]
-  end
-      endcase
-
-endif
-
-if toff_grid eq 'y' and calibrate eq 'y' and calibration_phase ne '3' then begin
-   a=min(abs(double(id[gg[g]])-cali_id_toff),ind)
-   t_offset=toff_data[ind]
-endif
+@procedures/calibration/apply_calibration_params.pro
 
 ; ---------------------
 ; define arrays
@@ -1079,7 +1058,7 @@ endif
 
 endfor                          ; RCPs
 
-                                ; GCMs
+endfor                          ; GCMs
 
 toc
 